a5870c314553449275cd7fd7d42c60c66742a3ac
carlosEA28 <carloseaduarte28@gmail.com>
2026-08-30 16:18:57 -0300

    feat: add initial project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ApartmentsModule } from './apartments/apartments.module';
+import { ResidentsModule } from './residents/residents.module';
+import { PackagesModule } from './packages/packages.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ApartmentsModule, ResidentsModule, PackagesModule, NotificationsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
